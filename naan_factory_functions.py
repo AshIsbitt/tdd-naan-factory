8267fed35e7e1a494edcb929b7003a4dbbc14e1d
Ash Isbitt <AIsbitt@tcadmins-MacBook-Air-2.local>
2020-03-05 17:15:11 +0000

    more stuff

--- naan_factory_functions.py
+++ naan_factory_functions.py
@@ -3,14 +3,11 @@
 # Functions aren't called here as this will violate the separation of concerns
 # and they will be run elsewhere
 
-def make_dough():
-    # As a user, I can use the make_dough with water
-# and flour to make dough.
+def make_dough(arg1, arg2):
+    return 'dough'
 
-def bake_dough():
-    # As a user, I can use the bake_dough with dough
-# to get naan.
+def bake_dough(arg1):
+    pass
 
-def run_factory():
-    #As a user, I can use the run_factory with water
-#and flour and get naan.
+def run_factory(arg1, arg2):
+    pass
--- test_naan_factory.py
+++ test_naan_factory.py
@@ -2,3 +2,10 @@
 
 from naan_factory_functions import *
 
+#1
+#As a user, I can use the make_dough with water
+#and flour to make dough.
+
+print("calling make_dough with water flour, expect 'dough")
+print(make_dough('wheat', 'flour') == 'dough')
+print('got: ', (make_dough('wheat', 'flour')))
